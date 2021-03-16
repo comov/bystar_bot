41ebd7c5135d3493c61377c68e833145ab1edb7c
felixx69 <68079339+felixx69@users.noreply.github.com>
2021-03-16 15:32:24 +0600

    Update assist.py

--- firstsec_bot/assist.py
+++ firstsec_bot/assist.py
@@ -1,16 +1,44 @@
-import telebot
-from telebot import types
-bot = telebot.TeleBot('1680362624:AAE0SSBiz5FOsfhLRAtUbyOb3SR2oHFpHzQ')
+from cgitb import text
+
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.types import ParseMode
+from aiogram.utils import executor
+from aiogram.utils.markdown import bold
+
+from config import TOKEN
 
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
 
-bot.polling(none_stop = True, interval = 0)
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Здравствуй пользователь, я твой личный помощник в этом мире!\nИспользуй /info, "
+                        "чтобы узнать список доступных команд!")
+
+
+@dp.message_handler(commands=['info'])
+async def process_info_command(message: types.Message):
+    await message.reply("Сейчас я могу ответить только на эти команды!\n /profile, /newprofile, /locate ")
+
+
+@dp.message_handler(commands=['profile'])
+async def process_profile_command(message: types.Message):
+    await message.reply('У тебя нет профиля!)')
+
+
+@dp.message_handler(commands=['newprofile'])
+async def process_newprofile_commnad(message: types.Message):
+    await message.reply('Прости, пока что ты не можешь создать профиль ;(((')
+
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
